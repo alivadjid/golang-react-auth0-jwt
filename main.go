b80b65b25f37c309ae37567791fef6aac5ce2f07
= <mshagidullin@rzdit.ru>
2022-08-20 17:18:52 +0300

    feat/api-add

--- main.go
+++ main.go
@@ -19,6 +19,27 @@ func main() {
 
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/statis/", http.FileServer(http.Dir("./static/"))))
 
+	// Our API is going to consist of three routes
+	// /status - which we will call to make sure that our API is up and runninng
+	// /products - which will retrieve a list of product that the user can leave feedback on
+	// /products/{slug}/feedback - which will capture user feedback on products
+
+	r.Handle("/status", NotImplemented).Methods("GET")
+
+	r.Handle("/products", NotImplemented).Methods("GET")
+
+	r.Handle("/products/{slug}/feedback", NotImplemented).Methods("POST")
+
+
+
 	// Our application will run on port 8080. Here we declare the port and pass in our router.
 	http.ListenAndServe(":8080", r)
 }
+
+// Here we are implementing the NotImplemented handler. Whener and API endpoint is hit
+// we will simply return the message "Not Implemented"
+
+var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+
+	w.Write([]byte("Not Implemented"))
+})
