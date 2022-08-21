69a4d6c9de4f5f0a18d787c361584163ef1435bf
= <mshagidullin@rzdit.ru>
2022-08-21 16:36:17 +0300

    feat/func to grab JSON web key set and return cerf

--- main.go
+++ main.go
@@ -185,3 +185,35 @@ var AddFeedbackHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Re
 	}
 })
 
+func getPemCert(token *jwt.Token)(string, error) {
+	cert := ""
+
+	resp, err := http.Get("https://YOUR_DOMAIN/.well-known/jwks.json")
+
+	if err != nil {
+		return cert, err
+	}
+
+	defer resp.Body.Close()
+
+	var jwks = Jwks{}
+
+	err = json.NewDecoder(resp.Body).Decode(&jwks)
+
+	if err != nil {
+		return cert, err
+	}
+
+	for k, _ := range jwks.Keys {
+		if token.Header["kid"] == jwks.Keys[k].Kid {
+			cert = "-----BEGIN CERTIFICATE-----\n" + jwks.Keys[k].X5c[0] + "\n-----END CERTIFICATE-----"
+		}
+	}
+
+	if cert == "" {
+		err := errors.New("Unable to find appropriate key.")
+		return cert, err
+	}
+
+	return cert, nil
+}
